a68f7ec95d585fcb31129a2cf24a69e800040c79
SR-45 <161393524+SR-45@users.noreply.github.com>
2024-09-18 01:07:17 +0100

    basic grid layout complete

---
+++ script.js
@@ -0,0 +1,22 @@
+let container = document.getElementById('container');
+
+function createGrid(size){
+    for (let index = 0; index < size; index++) {
+       // let row = document.createElement("div")
+        //row.classList.add("row");
+        for (let index = 0; index < size; index++) { 
+            const ratio = 100/size;
+            let div = document.createElement("div");
+            div.style.flex = "1 1 " + ratio +"%";
+            //div.style.width = "200px";
+           // div.style.height = "200px";
+           // row.appendChild(div);
+           container.appendChild(div);
+            
+        }
+       // container.appendChild(row);
+      
+    }
+}
+
+createGrid(10);
