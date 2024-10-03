ec140d521296d91155b389b199d1ff23863adddd
SR-45 <161393524+SR-45@users.noreply.github.com>
2024-10-04 00:24:07 +0100

    added colouring of grid

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-let container = document.getElementById('container');
+let container = document.getElementById('container')
 
-function createGrid(size){
+function createGrid(size) {
     for (let index = 0; index < size; index++) {
-       // let row = document.createElement("div")
-        //row.classList.add("row");
-        for (let index = 0; index < size; index++) { 
-            const ratio = 100/size;
+        for (let index = 0; index < size; index++) {
+            const ratio = 100 / size;
             let div = document.createElement("div");
             div.classList.add("box");
-            div.style.flex = "1 1 " + ratio +"%";
-            //div.style.width = "200px";
-           // div.style.height = "200px";
-           // row.appendChild(div);
-           container.appendChild(div);
-            
+            div.style.flex = "1 1 " + ratio + "%";
+
+            container.appendChild(div);
+
         }
-       // container.appendChild(row);
-      
+
     }
 }
 
-createGrid(10);
+function colourGrid(colour) {
+    boxes.forEach(function (box) {
+        box.addEventListener('mouseover', function () {
+            this.style.backgroundColor = colour
+            console.log("box")
+        })
+    }
+    )
+}
+
+function selectColour(){
+
+}
+
+
+createGrid(30);
+let boxes = document.querySelectorAll('div.box')
+
+colourGrid('red');
+document.getElementById('reset').addEventListener('click', function(){
+    boxes.forEach(function(box){
+        box.style.backgroundColor = 'white'
+    })
+    colourGrid('red')
+})
+
+document.getElementById('erase').addEventListener('click', function(){
+    colourGrid('white')
+})
