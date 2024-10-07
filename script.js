b9fc93560ce2b40ca199a580c02653fb43ea7153
SR-45 <161393524+SR-45@users.noreply.github.com>
2024-10-07 23:06:13 +0100

    changed reset logic

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ let curcolour = 'black'
 let prevcolour = 'black'
 let slider = document.getElementById('slider')
 let erase = false
-
+let boxes = document.querySelectorAll('div.box')
 
 function createGrid(size) {
+    container.innerHTML = ''
     for (let index = 0; index < size; index++) {
         for (let index = 0; index < size; index++) {
             const ratio = 100 / size;
@@ -19,17 +20,20 @@ function createGrid(size) {
         }
 
     }
+    boxes = document.querySelectorAll('div.box')
+    colourGrid()
 }
 
 function colourGrid() {
+
     boxes.forEach(function (box) {
         box.addEventListener('mousedown', function () {
             mouseDown = true
-            colourBox(box,curcolour)
+            colourBox(box, curcolour)
         })
         box.addEventListener('mouseover', () => {
             if (mouseDown) {
-                colourBox(box,curcolour)
+                colourBox(box, curcolour)
             }
         })
         document.addEventListener('mouseup', function () {
@@ -37,8 +41,9 @@ function colourGrid() {
 
         })
     }
-    
+
     )
+
 }
 
 function colourBox(box, colour) {
@@ -46,39 +51,49 @@ function colourBox(box, colour) {
 }
 
 createGrid(16);
-let boxes = document.querySelectorAll('div.box')
 
-colourGrid();
+
+
 
 document.getElementById('reset').addEventListener('click', function () {
     boxes.forEach(function (box) {
-        colourBox(box,'white')
+        colourBox(box, 'white')
+
+
     })
+    eraser.classList.remove('active')
+    erase = false
+    curcolour = 'black'
+    prevcolour = 'black'
+    value = 16
+    document.getElementById('value').textContent = value
+    createGrid(16)
 })
 
 let eraser = document.getElementById('erase')
 eraser.addEventListener('click', function () {
     erase = !erase
-    if(erase==true){
-    curcolour = 'white'
-    eraser.classList.add('active')} else {curcolour = prevcolour
+    if (erase == true) {
+        curcolour = 'white'
+        eraser.classList.add('active')
+    } else {
+        curcolour = prevcolour
         eraser.classList.remove('active')
     }
 
 })
 
 
-picker.addEventListener('input',(e)=>{
+picker.addEventListener('input', (e) => {
     curcolour = e.target.value
     prevcolour = e.target.value
     console.log(curcolour)
 })
 
-slider.addEventListener('input',(e)=>{
+slider.addEventListener('input', (e) => {
     value = e.target.value
     document.getElementById('value').textContent = value
-    container.innerHTML = ''
+
     createGrid(value)
     boxes = document.querySelectorAll('div.box')
-    colourGrid()
 })
